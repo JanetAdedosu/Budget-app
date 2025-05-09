b6f247e4b4d8ed2343e2277888025b4230475883
JanetAdedosu <adedosuadeola71@gmail.com>
2025-05-09 16:57:12 +0200

    sucessfully generated hive adapter for local storage

---
+++ budget_app/lib/models/expense.dart
@@ -0,0 +1,18 @@
+import 'package:hive/hive.dart';
+
+part 'expense.g.dart'; // Needed for code generation
+
+@HiveType(typeId: 0)
+class Expense extends HiveObject {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  double amount;
+
+  @HiveField(2)
+  DateTime date;
+
+  Expense({required this.title, required this.amount, required this.date});
+}
+
